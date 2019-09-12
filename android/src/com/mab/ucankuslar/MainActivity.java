eaf37e0e23f33015ca4d00d374d60936aa2ee7ea
Mehmet Ali BILGIN <mehmetalibilgin34@gmail.com>
2019-09-12 13:27:59 +0300

    no message

---
+++ android/src/com/mab/ucankuslar/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mab.ucankuslar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private PublisherAdView mPublisherAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-4055756653297968~7811372494");
+
+        PublisherAdView adView = new PublisherAdView(this);
+        adView.setAdSizes(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-4055756653297968~7811372494");
+
+        mPublisherAdView = findViewById(R.id.publisherAdView);
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+        mPublisherAdView.loadAd(adRequest);
+
+
+        mPublisherAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+        AdSize adSize = new AdSize(300, 50);
+    }
+}
